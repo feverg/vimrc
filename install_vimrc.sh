42df0ec8227f8d4e6c925c5ab0bbdb2915012ef6
feverg <fevergao@gmail.com>
2014-01-26 23:41:01 +0800

    Init vimrc

---
+++ install_vimrc.sh
@@ -0,0 +1,13 @@
+cd ~/.vim_runtime
+
+echo 'set runtimepath+=~/.vim_runtime
+
+source ~/.vim_runtime/vimrcs/basic.vim
+"source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
+
+source ~/.vim_runtime/vimrcs/my_configs.vim
+' > ~/.vimrc
+
+echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
